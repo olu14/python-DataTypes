56be122891339b2b1fd39b92d1667fb7cbae4c20
olu14 <77014859+olu14@users.noreply.github.com>
2022-02-20 18:30:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+
+
+
+#g = "stringy samply"
+#print(g[0:6])
+
+
+#==================
+#strins are immutable
+#a = "this is a sentence
+#a[2] = 0
+#print(a)
+
+'''
+numeric type(integer, float, complex)
+'''
+
+#x = 2012345
+#print(type(x))
+
+#y = 2.128938937
+#print(type(y))
+
+#z = 1 +2j
+#print(type(z))
+
+
+'''
+Mapping Type(Dictionary)
+'''
+
+#dict = {'name':'Kingsley', 'age':37}
+#print(dict['age'])
+
+#dict['age'] = 25
+#print(dict)
+
+'''
+Set Types
+'''
+#dictionarys have key-value pairs but sets have single values
+
+#set = {1,2,3,4}
+#print(type(set))
+
+#set can have mixed data types( all immutabe though)
+#set2 = {3.2, "hello", (1,2,3)}
+
+'''
+Boolean Type (True or False)
+'''
+print(type(True))
+print(True ==1)
+
+
+print(True and False)
